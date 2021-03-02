ba2de3f308fa7de6fc5c6a29f5ee0f7830270ac1
Andrew-M-C <laplacezhang@126.com>
2021-03-02 15:30:44 +0800

    [*] test: 添加公钥签名、私钥验签的示例代码

--- rsa/key_test.go
+++ rsa/key_test.go
@@ -1,7 +1,11 @@
 package rsa
 
 import (
+	"crypto"
 	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/hex"
 	"os"
 	"testing"
 )
@@ -22,16 +26,27 @@ func TestParseKey(t *testing.T) {
 
 	text := "Hello, RSA!"
 
-	sign, err := EncryptPKCS1v15PublicKey(rand.Reader, pub, []byte(text))
-	checkError(t, err, "EncryptPKCS1v15PublicKey")
+	// 加解密检查
+	ciphertext, err := EncryptPKCS1v15PrivateKey(rand.Reader, priv, []byte(text))
+	checkError(t, err, "EncryptPKCS1v15PrivateKey")
+	t.Logf("Got encrypted data: %s", hex.EncodeToString(ciphertext))
 
-	plain, err := DecryptPKCS1v15PrivateKey(rand.Reader, priv, sign)
-	checkError(t, err, "DecryptPKCS1v15PrivateKey")
+	plain, err := DecryptPKCS1v15PublicKey(pub, ciphertext)
+	checkError(t, err, "DecryptPKCS1v15PublicKey")
 
 	if s := string(plain); s != text {
 		t.Errorf("plain text mismatch, got '%s'", s)
 		return
 	}
+
+	// 签验签检查
+	h := sha256.Sum256([]byte(text))
+	ciphertext, err = rsa.SignPKCS1v15(rand.Reader, priv, crypto.SHA256, h[:])
+	checkError(t, err, "rsa.SignPKCS1v15")
+	t.Logf("Got sign: %s", hex.EncodeToString(ciphertext))
+
+	err = rsa.VerifyPKCS1v15(pub, crypto.SHA256, h[:], ciphertext)
+	checkError(t, err, "rsa.VerifyPKCS1v15")
 
 	t.Logf("check passed")
 	return
